704f3df486fc72515d9a5f630dd0b10c8c832136
Raul Gomez <raulgs96@hotmail.com>
2023-11-30 23:26:46 -0600

    functions

--- s1variables/main.js
+++ s1variables/main.js
@@ -1,1 +1,26 @@
-console.log("Hoola koders")
+
+//1.- Crear una función que me entregue la suma de dos números
+
+function addition(num1,num2){
+    let result = num1 + num2
+    return result
+}
+console.log(addition(5,5))
+
+//2,. Crear una función que evalúe si un número es par o impar
+
+function evenOrOdd (number){
+    let result
+    result = number%2===0 ?  "El numero es par" :  "el numero es impar"
+    console.log(result)
+}
+(evenOrOdd(5))
+
+//3,. Crear una función que escriba en consola letra por letra de un nombre
+function letterByLetter (name){
+    for (i=0; i<=name.length; i++){
+        console.log(name[i])
+    }
+}
+console.log(letterByLetter("Raul Gomez"))
+
