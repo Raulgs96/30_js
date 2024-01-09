3f080a625fc5795534cebfc55af95adda18cf106
Raul Gomez <raulgs96@hotmail.com>
2024-01-08 23:55:13 -0600

    pets

---
+++ pets/js/main.js
@@ -0,0 +1,77 @@
+let nameField = document.getElementById("name")
+let breedField = document.getElementById("breed")
+let imgField = document.getElementById("pet-image")
+
+let savePetButton = document.getElementById("save-pet")
+
+savePetButton.addEventListener("click", async (event) =>{
+    event.preventDefault();
+    let  name = nameField.value
+    let breed = breedField.value
+    let img = imgField.value
+
+    let pet = { name, breed, img}
+    console.log(pet)
+    let result = await savePet(pet)
+    console.log(result)
+    printAllPets(data)
+})
+const savePet = async (pet) => {
+    let response = await fetch(
+      "https://pets-32401-default-rtdb.firebaseio.com/.json",
+      {
+        method: "POST",
+        body: JSON.stringify(pet),
+      }
+    );
+    let data = await response.json();
+    return data;
+  };
+  const getAllPets = async () => {
+    let response = await fetch(
+      "https://pets-32401-default-rtdb.firebaseio.com/.json"
+    );
+    let data = await response.json();
+    printAllPets(data);
+  };
+  const createPetItem=(petData)=>{
+    let {name,breed,img} = petData
+    let petCard = document.createElement("div");
+    petCard.classList.add("card")
+    let imgCard = document.createElement("img")
+    imgCard.setAttribute("src", img)
+    imgCard.classList.add("card-img-top")
+    let bodyCard = document.createElement("div")
+    bodyCard.classList.add("card-body")
+    let nameCard = document.createElement("h5")
+    nameCard.classList.add("card-title")
+    let textCardName = document.createTextNode(`Nombre: ${ name}  `)
+    nameCard.append(textCardName)
+    let breedCard = document.createElement("h5")
+    breedCard.classList.add("card-title")
+    let textCardBreed = document.createTextNode(`Raza: ${ breed}  `)
+    breedCard.append(textCardBreed)
+    bodyCard.append(nameCard,breedCard)
+    petCard.append(imgCard,bodyCard)
+    
+    return petCard
+}
+
+const printAllPets = (pets) =>{
+    console.log(pets)
+    let petCards = document.getElementById("pet-cards")
+    petCards.innerHTML = ""
+
+    let petsArray = Object.keys(pets).map((key) => ({ ...pets[key], key }));
+    console.log(petsArray);
+
+    petsArray.forEach((pet) => {
+        let petItem = createPetItem(pet);
+        petCards.append(petItem);
+      });
+
+}
+ getAllPets()
+  
+   
+   
